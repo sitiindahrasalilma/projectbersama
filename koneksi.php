dfccbe8237361ecf8da541a6a231bd40b0afe070
dewinurulfadila <107034239+dewinurulfadila@users.noreply.github.com>
2022-08-04 13:25:44 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,15 @@
 <?php
 
-$link = ,ysqli_connect('localhost','root','','nilai')
+$server="localhost";
+$user="root";
+$pass="";
+$database="tbguru2";
+
+$koneksi = mysqli_connect("localhost","root","","dbguru2");
+if ($koneksi) {
+    $simpan =mysqli_select_db($koneksi, $database);
+    if (!$simpan);
+}else{
+}
+
 ?>
